b83e7dd5d7e4678c6d6e0fe00b1908a4f89c54de
mac <mac@macui-MacBookPro.local>
2024-05-09 08:51:45 -0700

    react-router-dom 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import { Route, Routes, Navigate } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <Routes>
+  <Route path="/" element={<h1>homepage</h1>}></Route>
+  <Route path="/posts" element={<h1>posts</h1>}></Route>
+  <Route path="/posts/:id" element={<h1>id</h1>}></Route>
+  <Route path="/posts/new" element={<h1>new</h1>}></Route>
+  <Route path="/posts/edit/:id" element={<h1>postedit</h1>}></Route>
+  <Route path="/posts/profile" element={<h1>profile</h1>}></Route>
+  <Route path="/posts/profile/edit" element={<h1>profile edit</h1>}></Route>
+  <Route path="/notifications" element={<h1>notifications</h1>}></Route>
+  <Route path="/notifications" element={<h1>postlist</h1>}></Route>
+  <Route path="/search" element={<h1>searchPage</h1>}></Route>
+  <Route path="/users/login" element={<h1>login</h1>}></Route>
+  <Route path="/users/signup" element={<h1>signup</h1>}></Route>
+  <Route path="*" element={<Navigate replace to="/"/>}/>
+</Routes>
   );
 }
 
